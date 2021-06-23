7cbc7506beff0390d6624f594be194be7a66c35a
tb2097 <travis.best@bronstudios.com>
2021-06-23 16:14:13 -0700

    uppdated libwacom-list-local-devices command to generate correct data if using newer version of libwacom

--- wacom-gui/wacom_data.py
+++ wacom-gui/wacom_data.py
@@ -117,16 +117,26 @@ class Tablets:
 
 
     def __get_libwacom_data(self):
-        p = subprocess.Popen("libwacom-list-local-devices --database %s" % self.db_path, shell=True,
-                             stdout=subprocess.PIPE)
-        output = p.communicate()[0].split('\n')
+        try:
+            # newer libwacom command
+            p = subprocess.Popen("libwacom-list-local-devices --database %s --format datafile" % self.db_path, shell=True,
+                                 stdout=subprocess.PIPE)
+            output = p.communicate()[0].split('\n')
+            if output[0].startswith("Unknown"):
+                # using libwacom < 1.x
+                p = subprocess.Popen("libwacom-list-local-devices --database %s" % self.db_path, shell=True,
+                                     stdout=subprocess.PIPE)
+                output = p.communicate()[0].split('\n')
+        except:
+            output = []
+            # TODO: this is fail state...
         cur_device = None
         buttons = False
         for line in output:
             if line == '[Device]':
                 cur_device = None
                 buttons = False
-            elif line.startswith('Name=') :
+            elif line.startswith('Name='):
                 cur_device = line.split('=')[1]
                 if cur_device in self.device_data:
                     cur_device = None
